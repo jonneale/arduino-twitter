6c38ea3cd3d45e1ba01019f718938b0bd79c44ad
Jon Neale <jon.neale@uswitch.com>
2011-03-23 09:48:45 +0000

    Refactored twitter client to only look for tweets more recent than the robot accounts last tweet. Moved requires out

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -1,19 +1,17 @@
-require 'twitter'
-require 'yaml'
 class TwitterClient
     
   def initialize
-    twitter_config = YAML.load_file('twitter_config.yml')
+    @twitter_config = YAML.load_file('twitter_config.yml')
     Twitter.configure do |config|
-      config.consumer_key = twitter_config[:apikey][:consumer_key]
-      config.consumer_secret = twitter_config[:apikey][:consumer_secret]
-      config.oauth_token = twitter_config[:oauth][:token]
-      config.oauth_token_secret = twitter_config[:oauth][:token_secret]
+      config.consumer_key = @twitter_config[:apikey][:consumer_key]
+      config.consumer_secret = @twitter_config[:apikey][:consumer_secret]
+      config.oauth_token = @twitter_config[:oauth][:token]
+      config.oauth_token_secret = @twitter_config[:oauth][:token_secret]
     end
-    @client = Twitter::Client.new    
+    @client = Twitter::Client.new 
   end
 
-  def tweet_update(action, from)
+  def tweet_update(action, from, id)
     update_messages = ["#{action} as commanded by #{from}",
                        "By your command, #{from}. #{action}",
                        "Acknowledged, #{from}. #{action}",
@@ -22,11 +20,22 @@ class TwitterClient
                        "I often think maybe this is robot hell. #{action} thanks to #{from}",
                        "#{action} on #{from}'s orders.",
                        "Understood, #{from}. #{action}. End of line"]
-    @client.update(update_messages[((rand * update_messages.length) - 1).to_i])
+    @client.update("#{update_messages[((rand * update_messages.length) - 1).to_i]} ##{id.to_s}")
+  end
+  
+  def last_tweet_id
+    @twitter_config[:lasttweet]
+  end
+  
+  def last_tweet_id=(value)
+    @twitter_config[:lasttweet] = value
   end
   
   def find_tweets_at_fwdbot
-    search = Twitter::Search.new
-    search.to("fwdbot")
+    user = Twitter.user("fwdbot")
+    search = Twitter::Search.new   
+    result = search.to("fwdbot").since_id(user.status.id).to_a
+    search.clear
+    result
   end
 end
